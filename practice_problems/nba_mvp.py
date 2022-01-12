c14b6c8c11d083526b20613833f92060e115600c
Stephen B <stephenb.k101010@null.net>
2022-01-12 11:41:59 -0700

    completion update

--- practice_problems/nba_mvp.py
+++ practice_problems/nba_mvp.py
@@ -11,5 +11,6 @@ with open("nba_finals.csv") as file:
     for mvp in mvp_list:
         if mvp_list.count(mvp) > 1:
             multi_mvp[mvp] = mvp_list.count(mvp)
-
+    times_won = multi_mvp.items()
+    print(times_won)
 #Get values, sort, remove dupes. Get key for each value and print.
