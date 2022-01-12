b7b03700a71751d1a59a0817afd15cdf2d7a53c5
Stephen B <stephenb.k101010@null.net>
2022-01-11 23:05:25 -0700

    final daily update maybe

--- practice_problems/nba_finals_by_team.py
+++ practice_problems/nba_finals_by_team.py
@@ -9,7 +9,8 @@ def get_years(team: str):
             finals_details[parts[0]] = parts[1]
     champ_years = []
     for entry in finals_details:
-        if finals_details[entry] == team: #This is not working. If I use "Miami Heat" instead of team, it works.
+        if finals_details[entry] == team: 
+            #This is not working. If I use "Miami Heat" instead of team, it works. Says team is the string parameter.
             champ_years.append(entry)
 
 print(get_years("Miami Heat"))
