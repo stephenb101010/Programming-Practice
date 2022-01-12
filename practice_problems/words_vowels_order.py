b63c26488b909b5b8b50a6e9d6e10a375f801ed4
Stephen B <stephenb.k101010@null.net>
2022-01-11 19:23:02 -0700

    completion update

--- practice_problems/words_vowels_order.py
+++ practice_problems/words_vowels_order.py
@@ -2,6 +2,17 @@ with open("sowpods.txt") as file:
     words = []
     for line in file:
         line = line.strip("\n")
-        if (line.count("a") == 1): 
-            words.append(line)
-    print(words)
+        if (line.count("A") == 1): #and (line.index("a") < line.index("e"))
+            if (line.count("E") == 1):
+                if (line.count("I") == 1):
+                    if (line.count("O") == 1):
+                        if (line.count("U") == 1):
+                            words.append(line)
+    filtered = []
+    for entry in words:
+        if entry.index("A") < entry.index("E"):
+            if entry.index("E") < entry.index("I"):
+                if entry.index("I") < entry.index("O"):
+                    if entry.index("O") < entry.index("U"):
+                        filtered.append(entry)
+    print(filtered)
