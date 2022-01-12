44be6296d2520ac0224f71d1656ed0eb657061ea
Stephen B <stephenb.k101010@null.net>
2022-01-11 23:31:36 -0700

    Brain quake update

--- practice_problems/nba_finals_by_team.py
+++ practice_problems/nba_finals_by_team.py
@@ -1,16 +1,17 @@
 def get_years(team: str):
+    finals_details = {}
+    champ_years = []
     with open("nba_finals.csv") as file:
-        finals_details = {}
         for line in file:
             line = line.strip("\n")
             parts = line.split(",")
             if parts[0] == "Year":
                 continue
-            finals_details[parts[0]] = parts[1]
-    champ_years = []
+            finals_details[parts[0]] = parts[1]               
     for entry in finals_details:
         if finals_details[entry] == team: 
             #This is not working. If I use "Miami Heat" instead of team, it works. Says team is the string parameter.
             champ_years.append(entry)
+    return champ_years
 
 print(get_years("Miami Heat"))
