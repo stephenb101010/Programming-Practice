0e5b27a63025c94675b762c05a252de9e7376d97
Stephen B <stephenb.k101010@null.net>
2022-01-11 19:43:29 -0700

    adding run time stats

--- practice_problems/baby_scrabble_list.py
+++ practice_problems/baby_scrabble_list.py
@@ -12,3 +12,18 @@ with open("baby_names_2020_short.txt") as names:
             if name == word:
                 matches.append(name)
 print(matches)
+
+#This code runs slower than baby_scrabble_set.py
+#Big O time for this code is O(n^2) and for the other is O(n)
+
+#time python3 baby_scrabble_set.py
+#{'LIAM', 'HENRY'}
+#real	0m0.260s
+#user	0m0.234s
+#sys	0m0.013s
+
+#$ time python3 baby_scrabble_list.py
+#['LIAM', 'HENRY']
+#real	0m1.042s
+#user	0m1.026s
+#sys	0m0.016s
--- practice_problems/baby_scrabble_set.py
+++ practice_problems/baby_scrabble_set.py
@@ -6,8 +6,22 @@ with open("sowpods.txt") as words, open("baby_names_2020_short.txt") as names:
         rev_word = word[::-1]
         reversed_words.add(rev_word)    
     for name in names:
-        name = name.strip("\n") #This works
-        upper_name = name.upper() #This works
-        names_set.add(upper_name) #This works
+        name = name.strip("\n")
+        upper_name = name.upper()
+        names_set.add(upper_name)
 reversed_words.intersection_update(names_set)
 print(reversed_words)
+#This code runs faster than baby_scrabble_list.py
+#Big O time for this code is O(n) and for the other is O(n^2)
+
+#time python3 baby_scrabble_set.py
+#{'LIAM', 'HENRY'}
+#real	0m0.260s
+#user	0m0.234s
+#sys	0m0.013s
+
+#$ time python3 baby_scrabble_list.py
+#['LIAM', 'HENRY']
+#real	0m1.042s
+#user	0m1.026s
+#sys	0m0.016s
