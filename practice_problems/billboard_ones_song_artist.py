abf010530f171f4f403b31931119ef570769cde8
Stephen B <stephenb.k101010@null.net>
2022-01-12 18:17:01 -0700

    Completed for real this time

--- practice_problems/billboard_ones_song_artist.py
+++ practice_problems/billboard_ones_song_artist.py
@@ -7,5 +7,6 @@ with open("billboard100_2000.csv") as file:
             continue
         if line[4] == "1":
             number_ones[line[1]] = line[2]
-    for key in number_ones:
+    print("These were the number one songs of 2000:")
+    for key in number_ones:        
         print(f'"{key}" - {number_ones[key]}')
