76f35520ff34c940016b859d74f4e063c5486b86
Stephen B <stephenb.k101010@null.net>
2022-01-12 17:35:35 -0700

    WIP routine update

--- practice_problems/countries_name_reuse.py
+++ practice_problems/countries_name_reuse.py
@@ -1,5 +1,17 @@
-with open("countries") as file:
+with open("countries.txt") as file:
     names = []
+    final = []
     for line in file:
         line = line.strip("\n")
-        names.append(line)
+        line = line.upper()
+        names.append(line) 
+    length = len(names)
+    for name in names:
+        i = 1
+        while i < length:
+            test = names[i]
+            if test.find(name) != -1:
+                final.append(name)
+                final.append(test)
+            i += 1
+print(final)
