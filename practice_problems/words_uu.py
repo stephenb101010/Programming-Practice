bdcebb081bd032eba7fcdb8abe000abfc62867e2
Stephen B <stephenb.k101010@null.net>
2022-01-14 17:04:03 -0700

    Additional comments

--- practice_problems/words_uu.py
+++ practice_problems/words_uu.py
@@ -2,8 +2,11 @@
 words = []
 key = "UU"
 with open("sowpods.txt") as file:
+    #words = [line.strip("\n") for line in file if key in line]
     for line in file:
-        line = line.replace("\n", "")
-        if line.find(key) != -1:
+        line = line.strip("\n")
+        #line = line.replace("\n", "")
+        #if line.find(key) != -1:
+        if key in line:
             words.append(line)
 print(words)
